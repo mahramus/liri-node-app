653a885402fc1b3a0f28aa01f42ff849b2a6699d
Anthony Mahramus <anthonym@anthonys-mbp.lan>
2018-04-07 09:48:47 -0400

    updated functions

--- liri.js
+++ liri.js
@@ -1,6 +1,29 @@
 require("dotenv").config();
 
-// Add the code required to import the keys.js file and store it in a variable.
+var bandList = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+var request = process.argv[2];
+var mediaName = process.argv[3];
+
+switch (request) {
+    case "my-tweets":
+      tweetFetch();
+      break;
+  
+    case "spotify-this-song":
+      spotifyFetch();
+      break;
+  
+    case "movie-this":
+      withdraw();
+      break;
+  
+    case "do-what-it-says":
+      lotto();
+      break;
+  };
+
+  
